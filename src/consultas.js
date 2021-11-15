c9d4d7c7331b0724238871a3125aec727b07ae6a
IdrissiBadr <bidr932@g.educaand.es>
2021-11-15 05:54:57 +0100

    Commit Src

--- src/consultas.js
+++ src/consultas.js
@@ -1,1 +1,113 @@
-/* https://www.shortlist.com/lists/40-best-film-adaptations */
+/* Mi madre tras leerse todos los libros de Stephen King quiere ver las adaptaciones que estén en Amazon Prime Video , 
+porque es la única plataforma de streaming que tiene , y que sea fiel al libro*/
+
+db.libros.find(
+    {
+        $and:[
+            {EscritorLibro: "Stephen King"},
+            {PlataformaStreamingPelicula: "Amazon Prime Video"},
+            {PeliculaFielAlLibro: true}
+        ]
+    }
+).pretty()
+
+/* Me gustaría regalarle a mi prima un libro que solo sea de Drama por su cumpleaños pero tampoco tengo mucho dinero que pueda gastar , como mucho 10 euros*/
+
+db.libros.find(
+    {
+        $and:[
+            {genero: 
+                {$eq: ["Drama"]},
+            },
+            {PrecioLibro: 
+                {$lte: 10}
+            }
+        ]
+    }
+).pretty()
+
+/* Estoy organizando una velada para ver películas pero mis amigos son muy cinéfilos y solo ven películas de los 70 y que sean de Terror o Aventuras */
+
+db.libros.find(
+    {
+        $and:[
+            {"Fechas.fechaPublicacionPelicula":
+                { $gte: new Date("1970-12-31")}
+            },
+            {$or:[
+                {genero: "Terror" },
+                {genero: "Aventuras" } 
+            ]}
+        ]
+    }
+)
+
+/* Hay dos formas de hacerlo , esta es con el $and implícito*/
+
+db.libros.find( {
+        $or:[
+                {genero: "Terror" },
+                {genero: "Aventuras" } 
+        ],
+        "Fechas.fechaPublicacionPelicula":
+                { $gte: new Date("1970-12-31") }
+        }
+)
+
+/* Sé que vi una pelicula adaptada de un libro pero el nombre del director no lo recuerdo , empezaba por S */
+
+db.libros.find(
+    {DirectorPelicula: {$regex: /^S,*/}} 
+).pretty()
+
+/* Mi padre no se fía de los Oscars , dice que son todos por contrato y solo se fía de los Globos de Oro */
+
+db.libros.find(
+    {
+     PremiosPelicula:{$elemMatch: {GlobosdeOro: 5}} 
+    }
+)
+
+/* Teniendo solo Netflix y siguiendo los premios Saturn este año , me apetece ver alguna pelicula galardonada con un Premio Saturn y que esté en Netflix */
+
+db.libros.find(
+    {
+        $and:[
+            {PlataformaStreamingPelicula:"Netflix"},
+            {PremiosPelicula:{$elemMatch:{Otros: "Saturn"}}}
+        ]
+    }
+)
+
+
+/* Al contrario de mi padre , mi hermano se fia de los Oscars , pero en este caso solo quiere ver peliculas que sean fieles al libro*/
+
+db.libros.find(
+     {
+      PremiosPelicula: {$elemMatch:{Oscars:{$gt: 0}}},
+      PeliculaFielAlLibro: true
+     }
+)
+
+/* Quiero ver una pelicula que no sea de Aventura , que sea fiel al libro , antes del 2000 y que este en Amazon Prime Video */
+
+db.libros.find(
+    {
+        genero: {$nin: ["Aventura"]},
+        PeliculaFielAlLibro: true,
+        "Fechas.fechaPublicacionPelicula": {$lte: new Date("1999-12-31")},
+        PlataformaStreamingPelicula: "Amazon Prime Video"
+    }
+)
+/* Solo quiero que salgan las peliculas que tengan una categoria de premio en Otros */
+
+db.libros.find({ PremiosPelicula: {$elemMatch:{Otros:{$size: 1}}}})
+
+/*Mi hermano quiere saber las peliculas que no solo tengan más de un Oscar , si no que tengan 2 premios en la categoría Otros */ 
+
+db.libros.find (
+    {
+        PremiosPelicula: {$elemMatch: {Oscars:{$gte: 0}}},
+        PremiosPelicula: {$elemMatch: {Otros:{$size: 2}}}
+    }
+)
--- src/inserts.js
+++ src/inserts.js
@@ -1,5 +1,6 @@
-db.libros.deleteMany({})
+db.libros.deleteMany({}),
 db.libros.insertMany(
+  [
     {
         tituloLibro: "El Padrino", 
         tituloPelicula: "El Padrino", 
@@ -10,13 +11,19 @@ db.libros.insertMany(
         },
         EscritorLibro: "Mario Puzo",
         DirectorPelicula: "Francis Ford Coppola",
-        PremiosPelicula: {
-            Oscars: 3,
-            GlobosdeOro: 5,
-            Otros: ["BAFTA","WGA","David Donatello"]
+        PremiosPelicula: [ 
+        {
+          Oscars: 3
         },
+        {
+          GlobosdeOro: 5
+        },
+        {
+          Otros: ["BAFTA","WGA","David Donatello"]
+        } ],
         PrecioLibro: 6.99,
         PlataformaStreamingPelicula: ["Amazon Prime Video","Orange TV","Vodafone TV"],
+        PeliculaFielAlLibro : true,
     },
     {
         tituloLibro: "El Silencio De Los Corderos", 
@@ -28,13 +35,19 @@ db.libros.insertMany(
         },
         EscritorLibro: "Thomas Harris",
         DirectorPelicula: "Jonathan Demme",
-        PremiosPelicula: {
-              Oscars: 5,
-              GlobosDeOro: 1,
-              Otros: ["BAFTA","Saturn"]
+        PremiosPelicula: [ 
+        {
+          Oscars: 5
         },
+        {
+          GlobosDeOro: 1
+        },
+        {
+          Otros: ["BAFTA","Saturn"]
+        } ],
         PrecioLibro: 6.32,
-        PlataformaStreamingPelicula: ["Amazon Prime Video","Filmin","Apple TV"], 
+        PlataformaStreamingPelicula: ["Amazon Prime Video","Filmin","Apple TV"],
+        PeliculaFielAlLibro : false,
     },
     {
         tituloLibro: "Matar a un Ruiseñor", 
@@ -46,13 +59,19 @@ db.libros.insertMany(
         },
         EscritorLibro: "Harper Lee",
         DirectorPelicula: "Robert Mulligan",
-        PremiosPelicula: {
-              Oscars: 3,
-              GlobosDeOro: 3,
-              Otros: ["WGA","David Donatello"]
+        PremiosPelicula: [ 
+        {
+          Oscars: 3
         },
+        {
+          GlobosDeOro: 3
+        },
+        {
+          Otros: ["WGA","David Donatello"]
+        } ],
         PrecioLibro: 4.99,
-        PlataformaStreamingPelicula: ["Apple TV"], 
+        PlataformaStreamingPelicula: ["Apple TV"],
+        PeliculaFielAlLibro : false,
     },
     {
         tituloLibro: "El Cuerpo", 
@@ -64,12 +83,115 @@ db.libros.insertMany(
         },
         EscritorLibro: "Stephen King",
         DirectorPelicula: "Rob Reiner",
-        PremiosPelicula: {
-              Oscars: 0,
-              GlobosDeOro: 2,
-              Otros: ["DGA","WGA"]
+        PremiosPelicula: [ 
+        {
+          Oscars: 0
         },
+        {
+          GlobosDeOro: 2
+        },
+        {
+          Otros: ["DGA","WGA"]
+        } ],
         PrecioLibro: 5.97,
         PlataformaStreamingPelicula: ["Amazon Prime Video","Filmin"],
-    }, 
-)
+        PeliculaFielAlLibro : true,
+    },
+    {
+      tituloLibro: "Carrie", 
+      tituloPelicula: "Carrie", 
+      genero: ["Suspense","Terror"],
+      Fechas:{
+        fechaPublicacionLibro: new Date("1974-04-05"), 
+        fechaPublicacionPelicula: new Date("1976-11-03"),
+      },
+      EscritorLibro: "Stephen King",
+      DirectorPelicula: "Brian De Palma",
+      PremiosPelicula: [ 
+      {
+        Oscars: 0
+      },
+      {
+        GlobosDeOro: 0
+      },
+      {
+        Otros: ["NSFC"]
+      } ],
+      PrecioLibro: 9.45,
+      PlataformaStreamingPelicula: ["Filmin"],
+      PeliculaFielAlLibro : false,
+    },
+    {
+      tituloLibro: "El Resplandor", 
+      tituloPelicula: "El Resplandor", 
+      genero: ["Drama","Terror"],
+      Fechas:{
+        fechaPublicacionLibro: new Date("1977-01-28"), 
+        fechaPublicacionPelicula: new Date("1980-12-19"),
+      },
+      EscritorLibro: "Stephen King",
+      DirectorPelicula: "Stanley Kubrick",
+      PremiosPelicula: [ 
+      {
+        Oscars: 0
+      },
+      {
+        GlobosDeOro: 0
+      },
+      {
+        Otros: ["Saturn","David Donatello"]
+      } ],
+      PrecioLibro: 10.4,
+      PlataformaStreamingPelicula: ["Amazon Prime Video","Netflix","HBO"],
+      PeliculaFielAlLibro : true,
+    },
+    {
+      tituloLibro: "Harry Potter y La Piedra Filosofal", 
+      tituloPelicula: "Harry Potter y La Piedra Filosofal", 
+      genero: ["Fantasía","Aventuras","Magia"],
+      Fechas:{
+        fechaPublicacionLibro: new Date("1997-06-30"), 
+        fechaPublicacionPelicula: new Date("2001-11-30"),
+      },
+      EscritorLibro: "J. K. Rowling",
+      DirectorPelicula: "Chris Columbus",
+      PremiosPelicula: [ 
+      {
+        Oscars: 0
+      },
+      {
+        GlobosDeOro: 0
+      },
+      {
+        Otros: ["BAFTA","Saturn"]
+      } ],
+      PrecioLibro: 12.34,
+      PlataformaStreamingPelicula: ["Amazon Prime Video","Netflix"],
+      PeliculaFielAlLibro : true,
+    },
+    {
+      tituloLibro: "El Señor De Los Anillos : La Comunidad Del Anillo", 
+      tituloPelicula: "El Señor De Los Anillos", 
+      genero: ["Fantasía","Aventuras"],
+      Fechas:{
+        fechaPublicacionLibro: new Date("1954-06-29"), 
+        fechaPublicacionPelicula: new Date("2001-12-19"),
+      },
+      EscritorLibro: "J. R. R. Tolkien",
+      DirectorPelicula: "Peter Jackson",
+      PremiosPelicula: [ 
+      {
+        Oscars: 4
+      },
+      {
+        GlobosDeOro: 4
+      },
+      {
+        Otros: ["BAFTA","Saturn"]
+      } ],
+      PrecioLibro: 18.95,
+      PlataformaStreamingPelicula: ["HBO","Apple TV"],
+      PeliculaFielAlLibro : true,
+    },
+  ]
+);
